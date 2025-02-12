9b57c49ae1160daa01acb99f670ea9fed092e330
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2025-02-12 12:02:59 -0500

    azure-blob-echo-cloud-account

---
+++ tf-modules/azure-blob-echo-cloud-account/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+provider "azurerm" {
+  features {}        
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  use_oidc        = true
+  oidc_token      = var.oidc_token
+}
+variable "oidc_token" {
+    type = string
+}
+variable "subscription_id" {
+    type = string
+    default = "test"
+}
+variable "tenant_id" {
+    type = string
+    default = "test"
+}
+variable "client_id" {
+    type = string
+    default = "test"
+}
+output "account" {
+  value = "hard-coded-account-from-repo"
+}
+output "container" {
+  value = "hard-coded-container-from-repo"
+}
